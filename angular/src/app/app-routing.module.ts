a0dbb20224420460c60ed98d1f772f2e035e99f4
Ryan <rgreenman23@gmail.com>
2020-05-15 11:00:15 -0400

    Added routes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CheckInComponent } from './check-in/check-in.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CashOutComponent } from './cash-out/cash-out.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'check-in', component: CheckInComponent },
+	{ path: 'checkout', component: CheckoutComponent },
+	{ path: 'cash-out', component: CashOutComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	exports: [RouterModule],
+	declarations: []
 })
 export class AppRoutingModule { }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CashOutComponent } from './cash-out/cash-out.component';
+import { CheckInComponent } from './check-in/check-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutComponent,
+    CashOutComponent,
+    CheckInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
